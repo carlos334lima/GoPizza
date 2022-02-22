7b101a58cc8b57087aed4fcf7f08841e9dfb0b04
Carlos Henrique de Lima <carlos334lima@gmail.com>
2022-02-21 22:42:16 -0300

    product

--- src/Screens/Product/index.tsx
+++ src/Screens/Product/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  Platform,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+  View,
+} from "react-native";
 
-// import { Container } from './styles';
+//@libraries
+import firestore from "@react-native-firebase/firestore";
+import storage from "@react-native-firebase/storage";
+
+//@components
+
+//@styles
+import {
+  Container,
+  Header,
+  Title,
+  DeleteLabel,
+  Upload,
+  PickImageButton,
+  Form,
+  Label,
+  InputGroup,
+  InputGroupHeader,
+  MaxCharacters,
+} from "./styles";
+
+type PizzaResponse = ProductProps & {
+  photo_path: string;
+  prices_sizes: {
+    p: string;
+    m: string;
+    g: string;
+  };
+};
 
 const Product = () => {
-  return <View />;
-}
+  return (
+    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+      <ScrollView showsVerticalScrollIndicator={false}></ScrollView>
+    </Container>
+  );
+};
 
-export  {Product};
+export { Product };
